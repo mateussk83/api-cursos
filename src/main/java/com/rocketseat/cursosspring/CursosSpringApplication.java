733089f183b7714f6d687a3a64d1d2471dc9852b
mateussk83 <mateussk83@gmail.com>
2024-01-26 10:47:24 -0300

    create project spring initializ+

---
+++ src/main/java/com/rocketseat/cursosspring/CursosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.cursosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/cursosspring/CursosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.cursosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
